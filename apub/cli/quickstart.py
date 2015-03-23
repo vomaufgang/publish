967ad98cba9eea80d34f4bd7127f514606ddf17d
Christopher Knörndel <cknoerndel@anited.de>
2015-03-23 06:06:55 +0100

    Reformat quickstart.py to conform with pep8

--- apub/cli/quickstart.py
+++ apub/cli/quickstart.py
@@ -22,29 +22,36 @@ focus_template = """from apub import Project, Book, Chapter
 
 
 def main():
-    project_id = input('Enter project id (may contain a-zA-Z0-9-_), will be used as part of the metadata file name: ')
+    project_id = input('Enter project id (may contain a-zA-Z0-9-_), will be '
+                       'used as part of the metadata file name: ')
     while project_id is None or not _id_pattern.match(project_id):
-        project_id = input('Invalid project id, please enter project id (may contain a-zA-Z0-9-_): ')
+        project_id = input('Invalid project id, please enter project id (may '
+                           'contain a-zA-Z0-9-_): ')
 
     book_title = input('Enter the book\'s title: ')
     while book_title is None or book_title.isspace():
-        book_title = input('Every book must have a title, please enter the book\'s title: ')
+        book_title = input('Every book must have a title, please enter the '
+                           'book\'s title: ')
 
-    book_subtitle = input('Enter the book\' subtitle (optional, leave empty to skip): ')
+    book_subtitle = input('Enter the book\' subtitle (optional, leave empty '
+                          'to skip): ')
     if book_subtitle is None or book_subtitle.isspace():
         book_subtitle = 'None'
     else:
         book_subtitle = "'{book_subtitle}'".format(book_subtitle=book_subtitle)
 
-    authors = input('Enter the author\'s name (separate multiple authors by comma): ')
+    authors = input('Enter the author\'s name '
+                    '(separate multiple authors by comma): ')
     first_chapter_name = input('Enter the name of the first chapter: ')
-    first_chapter_id = input('Enter the id of the first chapter (may contain a-zA-Z0-9-_): ')
+    first_chapter_id = input('Enter the id of the first chapter '
+                             '(may contain a-zA-Z0-9-_): ')
 
     # todo check for existing files or projects of the same id before continuing
 
     metadata_filename = '{project_id}_metadata.py'.format(project_id=project_id)
     if os.path.exists(os.path.join(os.getcwd(), metadata_filename)):
-        raise FileExistsError  # todo error message, project seems to already exist
+        raise FileExistsError
+        # todo error message, project seems to already exist
 
     with codecs.open(metadata_filename, 'w', 'utf-8') as metadata:
         metadata_file_contents = metadata_template.format(book_title=book_title,
