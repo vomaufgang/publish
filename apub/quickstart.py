d82bc86613532d4b55915156ade5721b36713f09
Christopher Knörndel <cknoerndel@anited.de>
2016-12-06 01:49:01 +0100

    Fix pep8 spacing

--- apub/quickstart.py
+++ apub/quickstart.py
@@ -17,8 +17,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import logging
-
+import logging.config
 log = logging.getLogger(__name__)
 log.addHandler(logging.NullHandler)
 
