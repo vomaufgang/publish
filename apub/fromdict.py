c6feb1be6f26184406b5de3da109dc5061c09d99
Christopher Knörndel <cknoerndel@anited.de>
2016-12-06 01:46:34 +0100

    Add logging block Document FromDict

--- apub/fromdict.py
+++ apub/fromdict.py
@@ -17,20 +17,57 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-# todo: Use for all input classes
-# todo: docstrings
-
 from abc import ABCMeta, abstractmethod
+
+import logging.config
+log = logging.getLogger(__name__)
+log.addHandler(logging.NullHandler())
 
 
 class FromDict(metaclass=ABCMeta):
+    """Abstract base class for classes that offer instantiation from
+    dictionaries where the properties of the inheriting class are
+    populated from the keys/values of a dictionary mirroring the class.
+
+    The abstract factory method 'from_dict' must be implemented
+    by the inheriting type.
+    """
+
     @classmethod
     @abstractmethod
     def from_dict(cls, dict_):
+        """Abstract method that must be implemented by inheriting classes.
+
+        Instantiates a new object from the providied dictionary 'dict_', using
+        the keys of 'dict_' to populate the attributes and properties of
+        the inheriting class.
+
+        Args:
+            dict_ (dict): The dictionary to build the new object out of.
+
+        Returns:
+            A new object created from the provided dictionary 'dict_'.
+        """
         raise NotImplementedError
 
     @classmethod
-    def get_attribute_from_dict(cls, attribute_name, dict_, default=None):
-        if attribute_name not in dict_:
+    def get_value_from_dict(
+            cls, key, dict_, default=None):
+        """Returns the value corresponding to the 'key' in 'dict_' or the
+        value of 'default' if the dictionary does not contain the key.
+
+        Args:
+            key (str): The key to return the corresponding value of.
+            dict_ (dict): The dictionary to get the value from.
+            default (optional): This value is returned if dict_
+                does not contain a key 'attribute_name'. Defaults to 'None'.
+
+        Returns: The value corresponding to the 'key' in 'dict_' or the
+            value of 'default' if the dictionary does not contain the key.
+        """
+        if dict_ is None:
+            raise AttributeError('dict_ must not be None.')
+
+        if key not in dict_:
             return default
         return dict_['attribute']
