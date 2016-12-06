e71e2bfd591ebde3a34f697c986a6b96c90f1768
Christopher Knörndel <cknoerndel@anited.de>
2016-12-06 01:41:22 +0100

    Fix pep8 newline placement

--- apub/substitution/__init__.py
+++ apub/substitution/__init__.py
@@ -17,7 +17,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-
 from .substitution import Substitution
 from .simple import SimpleSubstitution
 from .regex import RegexSubstitution
