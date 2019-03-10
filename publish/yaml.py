efdb2b59aa3c6dd4863a386a657930891eea22fe
Christopher Knörndel <cknoerndel@gmail.com>
2019-03-10 22:50:03 +0100

    Document every function in publish.yaml

--- publish/yaml.py
+++ publish/yaml.py
@@ -7,9 +7,7 @@
 # Distributed under the MIT License
 # (license terms are at http://opensource.org/licenses/MIT).
 
-""" todo yaml docstring
-todo: Make the -- optional for ebookconvert_params, add them automatically when missing
-todo: todo _load_outputs
+"""Load anited. publish projects from yaml strings.
 """
 import logging
 from typing import Dict, Tuple, Iterable, Union, List
@@ -27,14 +25,35 @@ YAML = ruamel.yaml.YAML(typ='safe', pure=True)
 
 
 def load_yaml(yaml: str) -> Dict:
-    """todo: docstring load_yaml"""
+    """Loads a yaml string into a Python dictionary.
+
+    Args:
+        yaml: The yaml string.
+
+    Returns:
+        The yaml structure as a Python dictionary.
+    """
     return YAML.load(yaml)
 
 
 def load_project(yaml: str) -> Tuple[Book,
                                      Iterable[Substitution],
                                      Iterable[Union[HtmlOutput, EbookConvertOutput]]]:
-    """todo: docstring load_project"""
+    """Loads a yaml string using the anited. publish project structure and returns the
+    components of the project as a tuple: the book, the substitutions and the outputs.
+
+    A book is always returned. Any properties of the book class not set in the yaml will be
+    left at their default values as defined in the book class.
+
+    If the yaml string does not contain any substitutions or outputs, empty lists are returned
+    in their place.
+
+    Args:
+        yaml: The yaml string.
+
+    Returns:
+        A tuple consisting of the book, the list of substitutions and the list of outputs.
+    """
     dict_ = load_yaml(yaml)
 
     book = _load_book(dict_)
@@ -45,7 +64,16 @@ def load_project(yaml: str) -> Tuple[Book,
 
 
 def _load_book(dict_: Dict) -> Book:
-    """todo: docstring _load_book
+    """Translates a dictionary into a book object. Dictionary keys and values are mapped to
+    corresponding book properties of the same name.
+
+    Dictionary keys that have no corresponding properties are omitted.
+
+    Args:
+        dict_: The dictionary.
+
+    Returns:
+        The book.
     """
     book = Book(**dict_)
 
@@ -53,7 +81,24 @@ def _load_book(dict_: Dict) -> Book:
 
 
 def _load_chapters(dict_: Dict) -> Iterable[Chapter]:
-    """todo: docstring _load_chapters
+    """Translates a dictionary into a list of chapter objects.
+
+    The dictionary is assumed to have the following structure::
+
+        {
+            'chapters': [{ 'src': 'some_file.md' },
+                         { 'src': '...' }]
+        }
+
+    If the key 'chapters' is not present in the dictionary or if there are no chapter
+    sub-dictionaries, an empty list is returned instead.
+
+    Args:
+        dict_: The dictionary.
+
+    Returns:
+        The list of chapter objects or an empty list either if not chapter sub-dictionaries are
+        present in the encapsulating dictionary or if the 'chapters' key itself is missing.
     """
     chapters = []
 
@@ -65,7 +110,34 @@ def _load_chapters(dict_: Dict) -> Iterable[Chapter]:
 
 
 def _load_substitutions(dict_: Dict) -> Iterable[Substitution]:
-    """todo: docstring _load_substitutions
+    """Translates a dictionary into a list of substitution objects.
+
+    The dictionary is assumed to have the following structure::
+
+        {
+            'substitutions': [{ 'old': 'some', 'new': 'text' },
+                              { 'pattern: '...', 'replace_with': '...' }]
+        }
+
+    If the key 'substitutions' is not present in the dictionary or if there are no substitution
+    sub-dictionaries, an empty list is returned instead.
+
+    The type of the substitution is inferred from the key names of the individual sub-dictionary.
+    The keys 'old' and 'new' will lead to the creation of a SimpleSubstitution and so on. Note that
+    the key names must match exactly. The keys 'old' and 'replace_with' inside the same
+    sub-dictionary will lead to a TypeError, because no Substitution class matches those property
+    names.
+
+    Args:
+        dict_: The dictionary.
+
+    Returns:
+        The list of chapter objects or an empty list either if not chapter sub-dictionaries are
+        present in the encapsulating dictionary or if the 'chapters' key itself is missing.
+
+    Raises:
+        TypeError: If the key names of a substitution dictionary don't match any Substitution
+            class implementation.
     """
     substitutions = []
 
@@ -87,9 +159,30 @@ def _load_substitutions(dict_: Dict) -> Iterable[Substitution]:
 
 
 def _load_outputs(dict_: Dict) -> Iterable[Union[HtmlOutput, EbookConvertOutput]]:
-    """todo: implement _load_outputs
-    todo: docstring _load_outputs
-    todo: document - stylesheet gets replaced, params get merged (local take precedence)
+    """Translates a dictionary into a list of output objects.
+
+    The dictionary is assumed to have the following structure::
+
+        {
+            'outputs': [{ 'path': 'some', 'new': 'text' },
+                        { 'path: '...', 'replace_with': '...' }]
+        }
+
+    If the key 'outputs' is not present in the dictionary or if there are no output
+    sub-dictionaries, an empty list is returned instead.
+
+    The type of the output is inferred from the file name provided as a value of the 'path' key
+    of the output sub-dictionary.
+
+    A file name ending in the file type '.html' will produce an HtmlOutput. '.epub', '.mobi' or
+    any other file type excluding '.html' will produce an EbookConvertOutput.
+
+    Args:
+        dict_: The dictionary.
+
+    Returns:
+        The list of output objects or an empty list either if not output sub-dictionaries are
+        present in the encapsulating dictionary or if the 'outputs' key itself is missing.
     """
     outputs = []
     global_stylesheet = None
